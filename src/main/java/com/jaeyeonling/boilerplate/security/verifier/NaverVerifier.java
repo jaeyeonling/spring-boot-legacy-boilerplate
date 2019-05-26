5651a87eec213fc64898028cb0b2e36b04f3b909
Jaeyeonling <jaeyeonling@gmail.com>
2019-05-26 13:00:44 +0900

    버그 수정

--- src/main/java/com/jaeyeonling/boilerplate/security/verifier/NaverVerifier.java
+++ src/main/java/com/jaeyeonling/boilerplate/security/verifier/NaverVerifier.java
@@ -1,6 +1,6 @@
 package com.jaeyeonling.boilerplate.security.verifier;
 
-import com.jaeyeonling.boilerplate.security.social.SocialFetchService;
+import com.jaeyeonling.boilerplate.security.social.naver.NaverFetchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 public class NaverVerifier extends SocialVerifier {
 
     @Autowired
-    public NaverVerifier(final SocialFetchService socialFetchService) {
-        super(socialFetchService);
+    public NaverVerifier(final NaverFetchService naverFetchService) {
+        super(naverFetchService);
     }
 }
